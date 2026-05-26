8334ff41ace822862fb60ca5372036ca5ca25233
VinkarioDev <rogeriobuenodev@gmail.com>
2026-05-26 17:01:25 -0300

    feat(tasks): adicionar estrutura inicial para edição de tarefas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,40 @@
 const button = document.querySelector("#botao");
 const ul = document.querySelector(".ul");
+const mytasks = [];
 
 button.addEventListener("click", () => {
-  const span = document.querySelector(".span");
   const li = document.createElement("li");
   const remove = document.createElement("button");
-  let contentInput = document.querySelector("#input").value;
+  const edit = document.createElement("button");
+  const divControls = document.createElement("div");
+  divControls.classList.add("divControls");
+  let contentInput = document.querySelector("#input");
 
-  li.innerHTML = contentInput;
-  remove.innerHTML = "x";
-  ul.appendChild(li);
+  if (contentInput.value === "") {
+    contentInput.classList.add("error-placeholder");
+    contentInput.placeholder = "Adicione uma Task!!";
+    contentInput.style.border = "1px solid red";
+  } else {
+    contentInput.classList.remove("error-placeholder");
+    contentInput.placeholder = "Digite a task";
+    contentInput.style.border = "1px solid #8100ba";
+    li.innerHTML = contentInput.value;
+    contentInput.value = "";
+    remove.innerHTML = "x";
+    edit.innerHTML = "edit";
+    ul.appendChild(li);
 
-  li.appendChild(remove);
+    li.appendChild(divControls);
+    divControls.appendChild(remove);
+    divControls.appendChild(edit);
+    remove.addEventListener("click", () => {
+      ul.removeChild(li);
+    });
 
-  remove.addEventListener("click", () => {
-    ul.removeChild(li);
-  });
+    edit.addEventListener("click", () => {
+       divControls.remove()
+      contentInput.value = li.textContent;
+     
+    });
+  }
 });
