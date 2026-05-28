4c37e8edfc7e6e8dadca7807c2919e2ad6a20288
VinkarioDev <rogeriobuenodev@gmail.com>
2026-05-28 14:30:01 -0300

    refatorando adicionando funcoes especificas pra cada tarefa

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,58 +1,84 @@
 const button = document.querySelector("#botao");
 const ul = document.querySelector(".ul");
-const mytasks = [];
 const form = document.querySelector(".form");
+let contentInput = document.querySelector("#input");
 
-const criaLi = () => document.createElement("li");
-const deleteButton = () => document.createElement("button");
-const editbutton = () => document.createElement("button");
-const saveEditButton = () => document.createElement("button");
-const divButtons = () => document.createElement("div");
-
-button.addEventListener("click", () => {
-  const remove = deleteButton();
-  const edit = editbutton();
-  const saveEdit = saveEditButton();
-  const divControls = divButtons();
-  const li = criaLi();
-  divControls.classList.add("divControls");
-  let contentInput = document.querySelector("#input");
+function adicionaTarefa() {
+  const divControls = document.createElement("div");
+  const remove = document.createElement("button");
+  const edit = document.createElement("button");
+  const li = document.createElement("li");
 
   if (contentInput.value === "") {
     contentInput.classList.add("error-placeholder");
     contentInput.placeholder = "Adicione uma Task!!";
-    contentInput.style.border = "1px solid red";
   } else {
     contentInput.classList.remove("error-placeholder");
     contentInput.placeholder = "Digite a task";
     contentInput.style.border = "1px solid #8100ba";
+
     li.innerHTML = contentInput.value;
     contentInput.value = "";
-    remove.innerHTML = "x";
-    edit.innerHTML = "edit";
-    ul.appendChild(li);
 
+    ul.appendChild(li);
     li.appendChild(divControls);
+
     divControls.appendChild(remove);
     divControls.appendChild(edit);
+    divControls.classList.add("divControls");
+    remove.innerHTML = "X";
+    edit.innerHTML = "EDIT";
+
     remove.addEventListener("click", () => {
-      ul.removeChild(li);
+      removeTarefa(li);
     });
 
     edit.addEventListener("click", () => {
-      contentInput.value = li.firstChild ? li.firstChild.nodeValue.trim() : "";
-      button.remove();
-      form.appendChild(saveEdit);
-      saveEdit.innerHTML = "Editar Tarefa";
-
-      saveEdit.addEventListener("click", (e) => {
-        e.preventDefault();
-        li.innerHTML = contentInput.value;
-        contentInput.value=''
-        saveEdit.remove();
-        li.appendChild(divControls)
-        form.appendChild(button);
-      });
+      editaTarefa(li);
     });
   }
+}
+
+function editaTarefa(liRef) {
+  const divControls = document.createElement("div");
+  const saveEdit = document.createElement("button");
+  const remove = document.createElement("button");
+  const edit = document.createElement("button");
+  contentInput.value = liRef.firstChild
+    ? liRef.firstChild.nodeValue.trim()
+    : "";
+  button.remove();
+  form.appendChild(saveEdit);
+  saveEdit.innerHTML = "Editar Tarefa";
+
+  saveEdit.addEventListener("click", (e) => {
+    e.preventDefault();
+    liRef.innerHTML = contentInput.value;
+    contentInput.value = "";
+    saveEdit.remove();
+    form.appendChild(button);
+    liRef.appendChild(divControls);
+    divControls.appendChild(remove);
+    divControls.appendChild(edit);
+    divControls.classList.add("divControls");
+    remove.innerHTML = "X";
+    edit.innerHTML = "EDIT";
+    remove.addEventListener("click", () => {
+      removeTarefa(liRef);
+    });
+
+    edit.addEventListener("click", () => {
+      editaTarefa(liRef);
+    });
+  });
+}
+
+function removeTarefa(liRef) {
+  liRef.remove();
+  contentInput.value = "";
+}
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  adicionaTarefa();
 });
